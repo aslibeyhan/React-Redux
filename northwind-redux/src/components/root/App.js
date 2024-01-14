bbbd0e943bd19752cee72ea6716fae0e675b2147
aslibeyhan <asli.beyhann@gmail.com>
2024-01-14 13:01:58 +0300

    reduxInro

---
+++ northwind-redux/src/components/root/App.js
@@ -0,0 +1,29 @@
+import { Container } from "reactstrap";
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
+import { Route, Routes } from "react-router-dom";
+import CartDetail from "../cart/CartDetail";
+import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
+import NotFound from "../common/NotFound";
+
+
+function App() {
+  return (
+    <div >
+     <Container>
+      <Navi/>
+      <Routes>
+        <Route path="/"  element={<Dashboard/>}/>
+        <Route path="/product"  element={<Dashboard/>}/>
+        <Route path="/saveproduct/:productId"  element={<AddOrUpdateProduct/>}/>
+        <Route path="/saveproduct"  element={<AddOrUpdateProduct/>}/>
+        <Route path="/cart"  element={<CartDetail/>}/>
+        <Route path="*" element={<NotFound />} />
+       
+      </Routes>
+     </Container>
+    </div>
+  );
+}
+
+export default App;
